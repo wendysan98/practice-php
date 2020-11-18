94edcc6feb754c545ea79e1aeec4af680e65ab19
Wendy Santoso <c14200036@john.petra.ac.id>
2020-11-18 18:50:09 +0700

    Menambahkan file php.php dan membuat sebuah code untuk menampilkan kata thank you diikuti dengan nama yang diinputkan user serta dapat kembali ke halaman utama setelah 5 detik

---
+++ php.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+    
+<div class="php">
+    <?php 
+    header("refresh:5;url=index.html");
+    $kalimat = "Thank you, ";
+    echo $kalimat;
+    echo $_POST["name"];
+    ?>
+</div>
+
+</body>
+</html>
